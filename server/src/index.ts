9e6729695bdc8eb32d93206f49cb7e1eeba6e157
Emmanuel Anca <anca.emmanuel96@gmail.com>
2026-05-22 18:57:13 -0300

    Limpiar appfrench1

--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as db from './db'; 
-import { fileURLToPath } from 'url';
-
-const isDirectRun = process.argv[1] === fileURLToPath(import.meta.url);
-if (isDirectRun) {
-    console.log(await db.puc_get_hierarchy(3));
-    console.log(await db.puc_get_id('1.1'));
-}
